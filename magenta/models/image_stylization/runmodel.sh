f6ed8db789a222ffaa01de993f8b189b28079f19
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-04 22:58:16 -0800

    Update runmodel.sh

--- magenta/models/image_stylization/runmodel.sh
+++ magenta/models/image_stylization/runmodel.sh
@@ -1,7 +1,7 @@
 image_stylization_transform \
-      --num_styles=<NUMBER_OF_STYLES> \
-      --checkpoint=/path/to/model.ckpt \
-      --input_image=/path/to/image.jpg \
-      --which_styles="[0,1,2,5,14]" \
-      --output_dir=/tmp/image_stylization/output \
-      --output_basename="stylized"
+      --num_styles=10 \
+      --checkpoint=/msshared/models/fastmodels/multistyle-pastiche-generator-monet.ckpt \
+      --input_image=/msshared/trainingtest_input/test1.jpg \
+      --which_styles="{0:0.1,1:0.1,2:0.1,3:0.1,4:0.1,5:0.1,6:0.1,7:0.1,8:0.1,9:0.1}" \
+      --output_dir=/msshared/trainingtest_output/ \
+      --output_basename="all_monet_styles"
